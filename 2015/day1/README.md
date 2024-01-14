6871f11347abb1654115a4601050dad5ed0c286b
Timo Laitila <timo.t.laitila@gmail.com>
2024-01-14 19:48:14 +0200

    2015 Day 1 solved with Nim and Zig

---
+++ 2015/day1/README.md
@@ -0,0 +1,7 @@
+# Advent of Code 2015 - Day 1
+* exercise: https://adventofcode.com/2015/day/1
+* input: https://adventofcode.com/2015/day/1/input
+
+### Implementations
+  * Nim: [day1.nim](day1.nim)
+  * Zig: [day1.zig](day1.zig)
---
+++ 2015/day1/day1.nim
@@ -0,0 +1,39 @@
+# To run:
+# nim r day1.nim
+
+const input_file = "input.txt"
+
+### Part 1
+
+# Read input
+import std/strutils
+
+const input = staticRead(input_file).strip()
+
+# Count the floors
+var floor = 0
+
+for c in input:
+  if c == '(':
+    floor += 1
+  elif c == ')':
+    floor -= 1
+
+echo "Santa is on floor ", floor
+# answer 138
+
+### Part 2
+
+floor = 0
+for position, character in input:
+    if character == '(':
+        floor += 1
+    if character == ')':
+        floor -= 1
+    
+    # Check if basement
+    if floor == -1:
+        echo position+1 # Add +1 to position, since started from 0, not 1
+        break
+    
+# answer 1771
